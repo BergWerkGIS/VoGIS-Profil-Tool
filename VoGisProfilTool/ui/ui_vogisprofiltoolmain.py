5f422b85bb938d8a870931c073f8d128a10309c7
Wilhelm Berg <wb@BergWerk-GIS.at>
2013-12-04 11:15:46 +0200

    v1.7.9

--- VoGisProfilTool/ui/ui_vogisprofiltoolmain.py
+++ VoGisProfilTool/ui/ui_vogisprofiltoolmain.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui/ui_vogisprofiltoolmain.ui'
 #
-# Created: Mon Dec  2 17:04:46 2013
+# Created: Wed Dec  4 11:15:10 2013
 #      by: PyQt4 UI code generator 4.9.1
 #
 # WARNING! All changes made in this file will be lost!
