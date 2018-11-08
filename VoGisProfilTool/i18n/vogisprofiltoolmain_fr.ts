9ec96ec9f96b6319a8ce6afaedfbbb59547e5568
Sylvain POULAIN <kikislater@users.noreply.github.com>
2018-11-08 14:48:18 +0400

    Add French translation
    Add French translation

---
+++ VoGisProfilTool/i18n/vogisprofiltoolmain_fr.ts
@@ -0,0 +1,385 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR" sourcelanguage="de_AT">
+<context>
+    <name>VoGISProfilToolMain</name>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolmain.py" line="225"/>
+        <source>Rastermodell(e) auswählen</source>
+        <translation>Choisir un(des) raster(s)</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolmain.py" line="228"/>
+        <source>Lage der Profillinie(n) festlegen</source>
+        <translation>Definir le(les) profil(s)</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolmain.py" line="232"/>
+        <source>Profillinie(n) aus Linienthema übernehmen</source>
+        <translation>Utiliser un profile depuis une couche ligne</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolmain.py" line="241"/>
+        <source>gerade Profillinie zwischen zwei Punkten</source>
+        <translation>Ligne droite entre deux points</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolmain.py" line="242"/>
+        <source>Stützstellen festlegen</source>
+        <translation>Definir les vertices</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolmain.py" line="233"/>
+        <source>Profillinie am Bildschirm zeichen</source>
+        <translation>Profil vectorisé</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolmain.py" line="229"/>
+        <source>Profillinie (neu) digitalisieren</source>
+        <translation>Digitaliser un (nouveau) profil</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolmain.py" line="230"/>
+        <source>nur selektierte Elemente verwenden</source>
+        <translation>Utiliser les objets sélectionnés uniquement</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolmain.py" line="244"/>
+        <source>Anzahl Profilpunkte pro Profil</source>
+        <translation>Nombre de vertices par profil</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolmain.py" line="243"/>
+        <source>Abstand zwischen den Profilpunkten</source>
+        <translation>Distnce entre les vertices</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolmain.py" line="224"/>
+        <source>VoGIS Profil Tool</source>
+        <translation>VoGIS Profile Tool</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolmain.py" line="226"/>
+        <source>Im aktuellen Extent sichtbare Raster auswählen</source>
+        <translation>Selectionner les rasters visibles dans l&apos;emprise actuelle</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolmain.py" line="227"/>
+        <source>Raster aktualisieren</source>
+        <translation>Actualiser la liste des couches rasters</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolmain.py" line="231"/>
+        <source>Linien explodieren (Multipart Features)</source>
+        <translation>Eclater les lignes (Géométries Multipart )</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolmain.py" line="234"/>
+        <source>Linie</source>
+        <translation>Ligne</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolmain.py" line="235"/>
+        <source>.</source>
+        <translation>.</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolmain.py" line="236"/>
+        <source>von:</source>
+        <translation>depuis:</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolmain.py" line="237"/>
+        <source>nach:</source>
+        <translation>vers:</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolmain.py" line="238"/>
+        <source>Rechtswert</source>
+        <translation>Valeur axe-X</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolmain.py" line="239"/>
+        <source>Hochwert</source>
+        <translation>Valeur axe-Y</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolmain.py" line="240"/>
+        <source>Zusammenhängende Linien verbinden</source>
+        <translation>Assembler les lignes connectées</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolmain.py" line="245"/>
+        <source>Profilpunkte erstellen an Knoten und Stützstellen</source>
+        <translation>Créer un profil de points à partir des noeuds et vertices</translation>
+    </message>
+</context>
+<context>
+    <name>VoGISProfilToolPlot</name>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolplot.py" line="118"/>
+        <source>VoGIS Profil Tool</source>
+        <translation>VoGIS Profile Tool</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolplot.py" line="119"/>
+        <source>Schließen</source>
+        <translation>Fermer</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolplot.py" line="120"/>
+        <source>DXF Linie</source>
+        <translation>Ligne DXF</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolplot.py" line="121"/>
+        <source>DXF Punkt</source>
+        <translation>Point DXF</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolplot.py" line="122"/>
+        <source>Shp Punkt</source>
+        <translation>Point Shapefile</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolplot.py" line="123"/>
+        <source>Shp Linie</source>
+        <translation>Ligne Shapefile</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolplot.py" line="124"/>
+        <source>Autocad Text</source>
+        <translation>Texte Autocad</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolplot.py" line="125"/>
+        <source>Dezimalzeichen (für Text, csv):</source>
+        <translation>Séparateur décimal (pour Text, csv):</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolplot.py" line="131"/>
+        <source>,</source>
+        <translation>,</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolplot.py" line="127"/>
+        <source>.</source>
+        <translation>.</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolplot.py" line="128"/>
+        <source>Spaltentrennzeichen (Text):</source>
+        <translation>Séparateur de Colonnes (Text):</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolplot.py" line="129"/>
+        <source>tab</source>
+        <translation>tab</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolplot.py" line="130"/>
+        <source>;</source>
+        <translation>;</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolplot.py" line="132"/>
+        <source>Hektometrie Attribute</source>
+        <translatorcomment>A traduire en mieux</translatorcomment>
+        <translation>Attributs de l&apos;hectomètre</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolplot.py" line="133"/>
+        <source>Linien Attribute</source>
+        <translation>Attributs des Lignes</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolplot.py" line="134"/>
+        <source>Textdatei</source>
+        <translation>FichierTexte</translation>
+    </message>
+    <message>
+        <location filename="ui/ui_vogisprofiltoolplot.py" line="135"/>
+        <source>csv (Excel)</source>
+        <translation>csv (Excel)</translation>
+    </message>
+</context>
+<context>
+    <name>code</name>
+    <message>
+        <location filename="vogisprofiltoolmaindialog.py" line="114"/>
+        <source>Kein Raster selektiert!</source>
+        <translation>Aucun raster sélectionné !</translation>
+    </message>
+    <message>
+        <location filename="vogisprofiltoolmaindialog.py" line="95"/>
+        <source>Keine Rasterebene vorhanden oder sichtbar! Nur hektometrieren?</source>
+        <translatorcomment>Besoin d&apos;une meilleure traduction</translatorcomment>
+        <translation>Aucune couche raster (visible) ! Créer des hectomètres seulement ?</translation>
+    </message>
+    <message>
+        <location filename="vogisprofiltoolmaindialog.py" line="121"/>
+        <source>Keine Profillinie vorhanden!</source>
+        <translation>Aucune ligne pour réaliser un profil !</translation>
+    </message>
+    <message>
+        <location filename="vogisprofiltoolmaindialog.py" line="135"/>
+        <source>Es konnten keine Profile erstellt werden.</source>
+        <translation>Impossible de créer des profils.</translation>
+    </message>
+    <message>
+        <location filename="vogisprofiltoolmaindialog.py" line="257"/>
+        <source>Profillinie digitalisieren abgebrochen!</source>
+        <translation>La Digitalisation a été annulée !</translation>
+    </message>
+    <message>
+        <location filename="vogisprofiltoolmaindialog.py" line="328"/>
+        <source>Der gewählte Layer hat keine selektierten Elemente.</source>
+        <translation>Aucun objet sélectionné dans la couche actuelle.</translation>
+    </message>
+    <message>
+        <location filename="vogisprofiltoolmaindialog.py" line="346"/>
+        <source>Rechtswert von</source>
+        <translation>Valeur-X depuis</translation>
+    </message>
+    <message>
+        <location filename="util/u.py" line="35"/>
+        <source>ist keine gültige Zahl!</source>
+        <translation>n&apos;est pas un nombre valide !</translation>
+    </message>
+    <message>
+        <location filename="vogisprofiltoolmaindialog.py" line="350"/>
+        <source>Hochwert von</source>
+        <translation>Valeur-Y depuis</translation>
+    </message>
+    <message>
+        <location filename="vogisprofiltoolmaindialog.py" line="354"/>
+        <source>Rechtswert nach</source>
+        <translation>Valeur-X vers</translation>
+    </message>
+    <message>
+        <location filename="vogisprofiltoolmaindialog.py" line="358"/>
+        <source>Hochwert nach</source>
+        <translation>Valeur-Y vers</translation>
+    </message>
+    <message>
+        <location filename="util/u.py" line="254"/>
+        <source>Konnte vorhandene Datei nicht löschen</source>
+        <translation>Impossible de supprimer un fichier existant</translation>
+    </message>
+    <message>
+        <location filename="util/u.py" line="263"/>
+        <source>Datei gespeichert.</source>
+        <translation>Fichier enregisté.</translation>
+    </message>
+    <message>
+        <location filename="util/u.py" line="264"/>
+        <source>Laden?</source>
+        <translation>Charger ?</translation>
+    </message>
+    <message>
+        <location filename="util/u.py" line="281"/>
+        <source>Treiber nicht verfügbar</source>
+        <translation>driver non disponible</translation>
+    </message>
+    <message>
+        <location filename="util/u.py" line="289"/>
+        <source>Konnte Datei nicht erstellen</source>
+        <translation>Impossible de créer le fichier</translation>
+    </message>
+    <message>
+        <location filename="util/u.py" line="300"/>
+        <source>Konnte Koordinatensystem nicht initialisieren!</source>
+        <translation>Impossible d&apos;initialiser le système de coordonnées !</translation>
+    </message>
+    <message>
+        <location filename="util/u.py" line="308"/>
+        <source>Konnte Layer nicht erstellen</source>
+        <translation>Impossible de créer la couche</translation>
+    </message>
+    <message>
+        <location filename="vogisprofiltoolplot.py" line="211"/>
+        <source>Punkt Shapefile exportieren</source>
+        <translation>Exporter en Point Shapefile</translation>
+    </message>
+    <message>
+        <location filename="vogisprofiltoolplot.py" line="213"/>
+        <source>Linien Shapefile exportieren</source>
+        <translation>Exporter en ligne Shapefile</translation>
+    </message>
+    <message>
+        <location filename="vogisprofiltoolplot.py" line="236"/>
+        <source>CSV-datei exportieren</source>
+        <translation>Exporter en fichier CSV</translation>
+    </message>
+    <message>
+        <location filename="vogisprofiltoolplot.py" line="266"/>
+        <source>Gleiches Dezimal- und Spaltentrennzeichen gewählt!</source>
+        <translation>Le séparateur décimal sélectionné est identique au séparateur de colonne !</translation>
+    </message>
+    <message>
+        <location filename="vogisprofiltoolplot.py" line="271"/>
+        <source>Textdatei exportieren</source>
+        <translation>Exporter en fichier texte</translation>
+    </message>
+    <message>
+        <location filename="vogisprofiltoolplot.py" line="298"/>
+        <source>AutoCad Textdatei exportieren</source>
+        <translation>Exporter en texte Autocad</translation>
+    </message>
+    <message>
+        <location filename="vogisprofiltoolplot.py" line="318"/>
+        <source>DXF exportieren</source>
+        <translation>Exporter en DXF</translation>
+    </message>
+    <message>
+        <location filename="vogisprofiltoolplot.py" line="360"/>
+        <source>Überhöhung</source>
+        <translation>Exaggeration</translation>
+    </message>
+    <message>
+        <location filename="util/createProfile.py" line="105"/>
+        <source>Multipart Feature vorhanden! Option zum Explodieren verwenden.</source>
+        <translation>Géométries Multipart détectées ! Sélectionner l&apos;option éclater svp.</translation>
+    </message>
+    <message>
+        <location filename="bo/profile.py" line="58"/>
+        <source>Profillaenge</source>
+        <translation>Longueur du Profil</translation>
+    </message>
+    <message>
+        <location filename="bo/profile.py" line="59"/>
+        <source>Segmentlaenge</source>
+        <translation>Lengueur du segment</translation>
+    </message>
+    <message>
+        <location filename="bo/profile.py" line="60"/>
+        <source>Rechtswert</source>
+        <translation>Valeur-X</translation>
+    </message>
+    <message>
+        <location filename="bo/profile.py" line="61"/>
+        <source>Hochwert</source>
+        <translation>Valeur-Y</translation>
+    </message>
+    <message>
+        <location filename="bo/profile.py" line="62"/>
+        <source>Profilnummer</source>
+        <translation>numéro du Profile</translation>
+    </message>
+    <message>
+        <location filename="bo/profile.py" line="63"/>
+        <source>Segmentnummer</source>
+        <translation>Numéro du Segment</translation>
+    </message>
+    <message>
+        <location filename="bo/profile.py" line="64"/>
+        <source>Punktnummer</source>
+        <translation>Numéro du Point</translation>
+    </message>
+    <message>
+        <location filename="bo/profile.py" line="65"/>
+        <source>Punktklasse</source>
+        <translation>Pointclass</translation>
+    </message>
+</context>
+</TS>
